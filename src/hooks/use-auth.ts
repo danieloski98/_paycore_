a123fdd3db0275edf4fe83fdf9407fe5c82edc7b
Neon <danielemmanuel257@gmail.com>
2026-07-05 14:17:41 +0100

    feat: enhance useCreateCompanyUserAccount hook with type safety and add schemas file

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -2,10 +2,12 @@ import { CreateCompanyUserAccountPayload } from '@/lib/auth/payload'
 import React from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { create_company_account } from '@/services/auth/auth-service';
+import { GeneralResponse } from '@/lib/types';
+import { AxiosResponse } from 'axios';
 
 export const useCreateCompanyUserAccount = (payload: CreateCompanyUserAccountPayload) => {
     // you can now make use of this hook in your components to create a company user account
-    const { isPending, mutate, error } = useMutation({
+    const { isPending, mutate, error } = useMutation<AxiosResponse<GeneralResponse<any>>>({
         mutationFn: async () => create_company_account(payload),
         mutationKey: ['create_company_user_account', payload],
     });
